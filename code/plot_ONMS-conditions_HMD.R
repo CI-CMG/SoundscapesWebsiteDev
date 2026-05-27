2ebd39505826dbffa2b4e609746a99eefc5c4ba2
Emma Beretta - NOAA Affiliate <emma.beretta@noaa.gov>
2026-05-26 19:28:34 -0700

    trying an interactive graph on site fk06

--- code/plot_ONMS-conditions_HMD.R
+++ code/plot_ONMS-conditions_HMD.R
@@ -32,14 +32,14 @@ rm(list=ls())
 # NRSsites "oc03", "hi00","ci05","sb09","as10","cb11","ch13","fgb06" 
 
 
-ONMSsites = c("gr01")
+ONMSsites = c("fk06")
 
 
 ## directories ####
-#outDir   =  "C:/Users/embe5980/SoundscapesWebsite/" # Emma local git repo 
+outDir   =  "C:/Users/embe5980/SoundscapesWebsite/" # Emma local git repo 
 #outDir   =  "F:/CODE/GitHub/SoundscapesWebsite/" # your local git repo 
 #outDir   =  "/Users/quca3108/SoundscapesWebsite/" # Quincy local git repo
-outDir = "X:/Emma_Beretta/SoundscapesWebsite/" #for GCP workstation remote desktop Emma
+#outDir = "X:/Emma_Beretta/SoundscapesWebsite/" #for GCP workstation remote desktop Emma
 #outDir   = "~/GitHub/SoundscapesWebsite/" #GCP WW
 
 
--- code/plot_ONMS-interactive-conditions_HMD.R
+++ code/plot_ONMS-interactive-conditions_HMD.R
@@ -170,15 +170,15 @@ pInt
 #make plot interactive
 interactive_plot <- ggplotly(pInt)
 
-p_interactive <- ggplotly(pInt, tooltip = "text")
-
 #remove hover info over grey shading
-interactive_plot$x$data[[14]]$hoverinfo <- "skip"
-
+#interactive_plot$x$data[[14]]$hoverinfo <- "skip"
 
 interactive_plot
 
 
+p_interactive <- ggplotly(pInt, tooltip = "text")
+
+p_interactive
 
 #  interactive_plot$x$layout$xaxis$type
 # interactive_plot$x$data[[18]]$textangle <- 90  
@@ -188,3 +188,137 @@ interactive_plot
 #  cat("TRACE", i, "\n")
 #print(interactive_plot$x$data[[i]][c("name","fill","mode","hoverinfo")])
 #}
+
+
+
+#TRYING NEW METHOD
+
+install.packages("ggiraph")
+
+library(ggiraph)
+
+
+
+
+#plot
+p = ggplot() +
+  #wind model
+  geom_line(data = mwindInfo[as.character(mwindInfo$windSpeed) == windUpp,], aes(x = variable, y = value), color = "black", linewidth = 1) +
+  geom_line(data = mwindInfo[as.character(mwindInfo$windSpeed) == windLow,], aes(x = variable, y = value), color = "black", linewidth = 1) +
+  scale_x_log10(labels = label_number(),limits = (c(10,fqupper)), guide = "axis_logticks") +  # Log scale for x-axis
+  
+  scale_color_manual(values = rev(colorRampPalette(c("darkblue", "lightblue"))(length(unique(summary$year))))) +
+  scale_fill_manual(values =  rev(colorRampPalette(c("darkblue", "lightblue"))(length(unique(summary$year))))) +
+  
+  # Add vertical lines at FOIs, label on right side
+  geom_vline(data = FOIs, aes(xintercept = FQstart, color = Label), linetype = "dashed", color = "black",linewidth = .5) +
+  geom_text(data = FOIs, aes(x = FQstart, y = label_height, label = Label), angle = 90, vjust = 1, hjust = 0.45, size = 4) +
+  geom_rect(data = FOIs, aes(xmin = FQstart, xmax = FQend, ymin = -Inf, ymax = Inf),
+            fill = "gray", alpha = 0.2) +  # Adjust alpha for transparency
+  
+  # Add vertical lines at FOIs, label on left side
+  geom_vline(data = FOIsL, aes(xintercept = FQstart, color = Label), linetype = "dashed", color = "black",linewidth = .5) +
+  geom_text(data = FOIsL, aes(x = FQstart, y = label_height, label = Label), angle = 90, vjust = 0, hjust = 0.5, size = 4) +
+  
+  # Add vertical set dash lines and grey shaded region at FOI ranges
+  #geom_vline(data = FOIsRange, aes(xintercept = FQstart, color = Label), linetype = "dashed", color = "red",linewidth = .5) +
+  #geom_vline(data = FOIsRange, aes(xintercept = FQend, color = Label), linetype = "dashed", color = "red",linewidth = .5) +
+  geom_rect(data = FOIsRange, aes(xmin = FQstart, xmax = FQend, ymin = -Inf, ymax = Inf), 
+            fill = "gray", alpha = 0.2)+  # Adjust alpha for transparency
+  geom_text(data = FOIsRange, aes(x = FQstart, y = label_height, label = Label), angle = 90, vjust = 1, hjust = 0.45, size = 4) +
+  
+  # Add vertical set dash lines and grey shaded region at FOI ranges, label on left
+  #geom_vline(data = FOIsRangeL, aes(xintercept = FQstart, color = Label), linetype = "dashed", color = "red",linewidth = .5) +
+  #geom_vline(data = FOIsRangeL, aes(xintercept = FQend, color = Label), linetype = "dotdash", color = "red",linewidth = .5) +
+  geom_rect(data = FOIsRangeL, aes(xmin = FQstart, xmax = FQend, ymin = -Inf, ymax = Inf), 
+            fill = "gray", alpha = 0.2)+  # Adjust alpha for transparency
+  geom_text(data = FOIsRangeL, aes(x = FQstart, y = label_height, label = Label), angle = 90, vjust = 0, hjust = 0.5, size = 4) +
+  
+  # geom_ribbon(data = mallData %>% pivot_wider(names_from = Quantile, values_from = SoundLevel),
+  #             aes(x = Frequency, ymin = `25%`, ymax = `75%`, fill = Year), alpha = 0.2) +
+  
+  #for the geom_ribbons below, if data only has one year (ch01 and fk08), comment out the first geom ribbon and change alpha of second from .3 to .1
+  geom_ribbon(data = mallData %>%
+                filter(Year != oldest_year) %>%
+                pivot_wider(names_from = Quantile, values_from = SoundLevel),
+              aes(x = Frequency, ymin = `25%`, ymax = `75%`, fill = Year),
+              alpha = 0.1) +
+  
+  #for the oldest year, make the shading darker since it is hard to see at alpha = .1 for lightblue
+  geom_ribbon(data = mallData %>% 
+                filter(Year == oldest_year) %>% 
+                pivot_wider(names_from = Quantile, values_from = SoundLevel),
+              aes(x = Frequency, ymin = `25%`, ymax = `75%`, fill = Year), 
+              alpha = 0.3) + # High alpha for visibility
+  
+  #median HMD values- each year
+  geom_line_interactive(data = mallData[mallData$Quantile == "50%",], 
+                        aes(x = Frequency, y = SoundLevel, color = Year, group = Year,
+                            # tooltip = paste0("Year: ", Year, "<br>Freq: ", 
+                            #                  Frequency, " Hz<br>Level: ", 
+                            #                  round(SoundLevel, 1), " dB"),
+                            tooltip = paste0("Year: ", Year),
+                            data_id = Year), 
+                        linewidth = 2) +
+  
+  #median HMD values- all data
+  geom_line_interactive(data = mALL[mALL$Quantile == "50%",], 
+                        aes(x = Frequency, y = SoundLevel, group = 1,
+                                                                  
+                            # tooltip = paste0("All Years Median<br>Freq: ", 
+                            #                  Frequency, " Hz<br>Level: ", 
+                            #                  round(SoundLevel, 1), " dB"),
+                            tooltip = paste0("All Years Median"),                                      
+                            data_id = "all"), 
+                        color = "black", linewidth = 1,
+                            linetype = "dotted") +
+  
+  scale_y_continuous(limits = c(30, NA)) +  # use to manually scale y minimum so vert line labels are visible
+  
+  # Additional aesthetics
+  theme_minimal() +
+  labs(
+    #title = paste0(toupper(site), "(",siteInfo$`Oceanographic category`, ")"), 
+    caption  = caption_text,
+    color = legend_label,        #IF biological then change to Year*
+    fill = legend_label,        #IF biological then change to Year*
+    x = "Frequency Hz",
+    y = expression(paste("Sound Levels (dB re 1 ", mu, " Pa"^2, "/Hz)" ) ),
+    subtitle = subtitle_text) +
+  theme(legend.position = "right",
+        plot.caption = ggtext::element_markdown(hjust = 0, size = 12),
+        axis.title.x = element_text(size = 14),           # X-axis label size
+        axis.title.y = element_text(size = 14),           # Y-axis label size
+        axis.text = element_text(size = 14),
+        legend.text = element_text(size = 12),
+        axis.ticks.length.x = unit(0.25, "cm"), 
+        axis.ticks.x = element_line(color = "grey", linewidth = 0.3), 
+        axis.line.x = element_line(color = "grey", linewidth = 0.3)    
+  ) 
+p
+
+
+
+g <- girafe(ggobj = p,
+       width_svg = 10,
+       height_svg = 10,
+       options = list(
+         opts_hover(css = "stroke-width:3;opacity:1;"),
+         opts_hover_inv(css = "opacity:0.2;"),
+         opts_tooltip(css = "background:white;padding:6px;border-radius:4px;border:1px solid #ccc;font-size:12px;"),
+         opts_sizing(rescale = TRUE),
+         opts_selection(type = "multiple", css = "opacity:1;"),
+         opts_selection_inv(css = "opacity:0.1;")  # fades unselected years
+       ))
+
+install.packages("htmlwidgets")
+library(htmlwidgets)
+
+saveWidget(g, file = "myFK06plot.html", selfcontained = TRUE)
+
+getwd()
+
+
+
+ggplotly(p)
+
