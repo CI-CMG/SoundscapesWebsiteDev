a063563d37fc0b8298300251a8628d5f74d62af8
Emma Beretta - NOAA Affiliate <emma.beretta@noaa.gov>
2026-05-28 10:10:49 -0700

    fixing cutoff?

--- content/pythonFunctions/generalFunctions.py
+++ content/pythonFunctions/generalFunctions.py
@@ -180,10 +180,10 @@ def addPlotly(sourceHTML):
                 src="resources/{sourceHTML}"
                 name="targetframe"
                 allowTransparency="true"
-                # scrolling="no"
+                scrolling="auto"
                 frameborder="0"
-                width="700px"
-                height="850px"
+                width="100%"
+                height="950px"
             >
             </iframe>
 			"""
