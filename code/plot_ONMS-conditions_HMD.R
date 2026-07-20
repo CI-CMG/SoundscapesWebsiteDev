11921d1e064cc9c810a5e5bf760027f43e91c88a
Emma Beretta - NOAA Affiliate <emma.beretta@noaa.gov>
2026-07-20 22:52:42 +0000

    NE FOI fix?

--- code/plot_ONMS-conditions_HMD.R
+++ code/plot_ONMS-conditions_HMD.R
@@ -34,16 +34,16 @@ rm(list=ls())
 #NMFS "ne01" #site names: "cox01","cox03","ns02","ns05","ns08","ustr06","ustr09"
 
 
-ONMSsites = c("cox01","ns02")
+ONMSsites = c("cox01")
 
 
 ## directories ####
 #outDir   =  "C:/Users/embe5980/SoundscapesWebsite/" # Emma local git repo 
 #outDir   =  "F:/CODE/GitHub/SoundscapesWebsite/" # your local git repo 
 #outDir   =  "/Users/quca3108/SoundscapesWebsite/" # Quincy local git repo
-#outDir = "X:/Emma_Beretta/SoundscapesWebsite/" #for GCP workstation remote desktop Emma
+outDir = "X:/Emma_Beretta/SoundscapesWebsiteDev/" #for GCP workstation remote desktop Emma
 #outDir   = "~/GitHub/SoundscapesWebsite/" #GCP WW
-outDir   = "~/GitHub/SoundscapesWebsiteDev/" #GCP WW
+#outDir   = "~/GitHub/SoundscapesWebsiteDev/" #GCP WW
 
 
 outDirG  =  paste0(outDir,"content/resources/") #where save graphics
@@ -223,10 +223,12 @@ for (uu in 1:length(ONMSsites)) { # uu = 1
 #  FOIs <- FOIs [FOIs$Site == toupper(site5) | is.na(FOIs$Site), ]
 #  }
   
-
-    FOIs <- FOIs [FOIs$Site == toupper(site5) | is.na(FOIs$Site), ]
   
-  
+  if (site5 == "ne") {
+    FOIs <- FOIs[FOIs$Site == toupper(site3) | is.na(FOIs$Site), ]
+  } else {
+    FOIs <- FOIs[FOIs$Site == toupper(site5) | is.na(FOIs$Site), ]
+  }
   
   ##frequency(s) to track
   #any rows with blank site column apply to all sites in NMS, any rows with just on site apply only to that site
