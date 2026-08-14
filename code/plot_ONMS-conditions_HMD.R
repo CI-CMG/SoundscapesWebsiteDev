97e589aac08d657ddf2eecd3b9c43c89ae1aef0b
Samara Haver <9533474+sahav@users.noreply.github.com>
2026-08-14 20:25:48 +0000

    NE plot updates

--- code/plot_ONMS-conditions_HMD.R
+++ code/plot_ONMS-conditions_HMD.R
@@ -34,7 +34,7 @@ rm(list=ls())
 #NMFS NE #site names: "cox01","cox03","ns02","ns05","ns08","ustr06","ustr09","ne08"
 
 
-ONMSsites = c("b")
+ONMSsites = c("cox01","cox03","ns02","ns05","ns08")
 
 
 ## directories ####
