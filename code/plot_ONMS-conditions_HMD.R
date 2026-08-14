11aa98d628eab083776f3631dfde072dea35f843
Samara Haver <9533474+sahav@users.noreply.github.com>
2026-08-14 23:17:15 +0000

    NE plot renaming

--- code/plot_ONMS-conditions_HMD.R
+++ code/plot_ONMS-conditions_HMD.R
@@ -34,7 +34,7 @@ rm(list=ls())
 #NMFS NE #site names: "cox01","cox03","ns02","ns05","ns08","ustr06","ustr09","ne08"
 
 
-ONMSsites = c("cox01","cox03","ns02","ns05","ns08")
+ONMSsites = c("cox01","cox03","ns02","ns08")
 
 
 ## directories ####
