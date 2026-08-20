237e9a961e02356d5339266a262d168d6f6afa72
Quincy Cantu <quincyscantu@gmail.com>
2026-08-20 12:15:17 -0600

    Removed extra 'content' directory in plotly path

--- content/pythonFunctions/generalFunctions.py
+++ content/pythonFunctions/generalFunctions.py
@@ -175,7 +175,7 @@ def makeImage(imageName, identifier, width=700, altText=""):
     return initialImage
 
 def addPlotly(sourceHTML, site="", identifier=""):
-    inputDir = "content/resources"
+    inputDir = "resources"
     path = f'{inputDir}/{sourceHTML}'
     return f'<iframe src="{path}" width="100%" height="900px" style="border:none id="{site}{identifier}";"></iframe>'
 #     return f'''
